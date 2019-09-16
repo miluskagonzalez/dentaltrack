4c25b94dfaac4fbd75cc917d75148b32d8f78c9f
Miluska González <miluska.gonzalezar@gmail.com>
2019-09-15 19:01:37 -0500

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import Login from './componentes/Login'
+
+function App() {
+  return (
+    <div >
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Login.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from 'react';
+import logo from '../images/logo-small.png'
+
+class Login extends Component {
+  render () {
+    return (
+    <Fragment>
+      <main className="flex-container">
+        <div className="container">
+          <div className="section center-align">
+            <img src={logo} alt=""/>
+          </div>
+          <h6 className="center-align brand-text"><b>INICIAR SESIÓN</b></h6>
+          <form className="section row">
+            <div className="input-field col s12 m6 offset-m3 l4 offset-l4">
+              <input type="text" />
+              <label>Usuario</label>
+            </div>
+            <div className="input-field col s12 m6 offset-m3 l4 offset-l4">
+              <input type="password" />
+              <label>Contraseña</label>
+            </div>
+            <div className="input-field col s12 center-align">
+              <div className="waves-effect waves-light btn brand-bg">INICIAR SESIÓN</div>
+            </div>
+          </form>
+          <div className="center-align">
+            <a href="">¿Olvidaste tu contraseña?</a><br/>¿No tienes cuenta? <a href="">Registrate</a>
+          </div>
+        </div>
+      </main>
+      <footer className="fixed-footer center-align">
+        <p><small>Software DentalTrack v.1 by Agencia Misterio 2019</small></p>
+      </footer>
+    </Fragment>
+    )
+  }
+}
+
+export default Login
