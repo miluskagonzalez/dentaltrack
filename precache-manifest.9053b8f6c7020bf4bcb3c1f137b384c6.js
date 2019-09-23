250168d0938037588cf0ed3c869a01b8943c1f24
Miluska Gonzalez <miluska.gonzalez@arkanosoft.com>
2019-09-23 10:42:50 -0500

    Updates

---
+++ precache-manifest.9053b8f6c7020bf4bcb3c1f137b384c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2043fd374a980dfbe72ea78f774cfd",
+    "url": "/dentaltrack/index.html"
+  },
+  {
+    "revision": "1bd5eba425ed60e02a95",
+    "url": "/dentaltrack/static/css/2.2050375e.chunk.css"
+  },
+  {
+    "revision": "8cfdf126dbace5297d76",
+    "url": "/dentaltrack/static/css/main.42b411c8.chunk.css"
+  },
+  {
+    "revision": "1bd5eba425ed60e02a95",
+    "url": "/dentaltrack/static/js/2.260ebaf9.chunk.js"
+  },
+  {
+    "revision": "8cfdf126dbace5297d76",
+    "url": "/dentaltrack/static/js/main.a92ac3d6.chunk.js"
+  },
+  {
+    "revision": "25ea7e491d43b53a187d",
+    "url": "/dentaltrack/static/js/runtime~main.c30f1502.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/dentaltrack/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/dentaltrack/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/dentaltrack/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/dentaltrack/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
